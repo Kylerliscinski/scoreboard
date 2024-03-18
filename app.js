796605110cbb7e0087a2de136aec196159d760db
Kylerliscinski <62256404+Kylerliscinski@users.noreply.github.com>
2024-03-18 15:57:15 -0600

    2nd commit

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
-function resetButton() {
-  console.log("Button Clicked")
+let homeScore = 0
+let awayScore = 0
+let reset = ""
+let value = 0
+
+function homeAdd1Button() {
+  homeScore++
+  console.log("Score Increase", homeScore)
+  const homeScoreElem = document.getElementById('homeScore')
+  homeScoreElem.innerText = homeScore.toString()
 }
 
-function add1Button() {
+function awayAdd1Button() {
+  awayScore++
+  console.log("Score Increase", awayScore)
+  const awayScoreElem = document.getElementById('awayScore')
+  awayScoreElem.innerText = awayScore.toString()
+}
 
+function homeAdd3Button() {
+  homeScore += 3
+  console.log("Score Increase", homeScore)
+  const homeScoreElem = document.getElementById('homeScore')
+  homeScoreElem.innerText = homeScore.toString()
+}
+
+function awayAdd3Button() {
+  awayScore += 3
+  console.log("Score Increase", awayScore)
+  const awayScoreElem = document.getElementById('awayScore')
+  awayScoreElem.innerText = awayScore.toString()
+}
+
+function resetButton() {
+  const reset = document.getElementById('homeScore')
+  reset.innerText = homeScore.toString()
 }
